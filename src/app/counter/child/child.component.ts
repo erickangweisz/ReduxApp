cc18034687d07c0abe91079e090477ffaec8bf09
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-21 22:06:35 +0000

    send counter value from child to parent using EventEmitter and Output

--- src/app/counter/child/child.component.ts
+++ src/app/counter/child/child.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,14 +9,17 @@ import { Component, Input } from '@angular/core';
 export class ChildComponent {
 
   @Input() counter: number;
+  @Output() counterChange = new EventEmitter<number>();
 
   constructor() {}
 
   multiply(): void {
     this.counter *= 2;
+    this.counterChange.emit(this.counter);
   }
 
   divide(): void {
     this.counter /= 2;
+    this.counterChange.emit(this.counter);
   }
 }
