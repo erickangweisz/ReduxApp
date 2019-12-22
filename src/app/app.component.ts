37f0426457dac1099ad244866630dbae2c29e118
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-22 17:17:33 +0000

    implement and dispatch actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+
+interface AppState {
+  counter: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,15 +13,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   counter: number;
 
-  constructor() {
-    this.counter = 10;
+  constructor(private store: Store<AppState>) {
+    //this.counter = 10;
+    this.store.subscribe(state => {
+      this.counter = state.counter;
+    });
   }
 
   increment(): void {
-    this.counter++;
+    //this.counter++;
+    const action: Action = {
+      type: 'INCREMENT'
+    };
+    this.store.dispatch(action);
   }
 
   decrement(): void {
-    this.counter--;
+    //this.counter--;
+    const action: Action = {
+      type: 'DECREMENT'
+    };
+    this.store.dispatch(action);
   }
 }
