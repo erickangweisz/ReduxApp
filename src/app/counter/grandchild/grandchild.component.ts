a87f3972da1f1f5aae17c23baeb2d04361e5eaf1
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-22 09:15:41 +0000

    add grandchild to child component and remove OnInit function

--- src/app/counter/grandchild/grandchild.component.ts
+++ src/app/counter/grandchild/grandchild.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grandchild',
   templateUrl: './grandchild.component.html',
   styles: []
 })
-export class GrandchildComponent implements OnInit {
+export class GrandchildComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
